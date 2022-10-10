0dea5f5c36287fb4b1493478a312810253d284ef
Anil <53553840+anil-bit@users.noreply.github.com>
2022-10-10 09:47:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
         requirement_list = None
         with open(requirement_file_name) as requirement_file:
             requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
+            print(requirement_list)
             requirement_list.remove(REMOVE_PACKAGE)
         return requirement_list
     except Exception as e:
